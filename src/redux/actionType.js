3fd1549009c2244d41fdadd9938466b93934730d
walda8 <74023511+walda8@users.noreply.github.com>
2021-01-10 21:57:56 +0100

    Update actionType.js

--- src/redux/actionType.js
+++ src/redux/actionType.js
@@ -3,3 +3,6 @@ export const EDITTASK = "EDITTASK";
 export const COMPLETETASK = "COMPLETETASK";
 export const DELETETASK = "DELETETASK";
 export const CANCEL = "CANCEL";
+export const FILTERDONE = "FILTERDONE";
+export const FILTERNOTDONE = "FILTERNOTDONE";
+export const FILTERALL = "FILTERALL";
