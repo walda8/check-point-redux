63aca206bc6e42d4f6f8e946d23a977a569b66ea
walda8 <74023511+walda8@users.noreply.github.com>
2021-01-13 14:28:29 +0100

    Update ListTask.js

--- src/components/ListTask.js
+++ src/components/ListTask.js
@@ -3,10 +3,17 @@ import Todo from './Task';
 import { useSelector } from "react-redux";
 
 function ToDoList() {
-    const tasks = useSelector(state => state)
+    const task = useSelector(state => state)
     return (
         <div>
-            {tasks.map(el => <Todo key={el.id} el={el} />)}
+            {
+            task.str=='ALL' ?
+            task.t.map(el => <Todo key={el.id} el={el} />):
+            task.str=='DONE' ?
+            task.t.filter(el=>el.isDone).map(el => <Todo key={el.id} el={el} />):
+            task.t.filter(el=>!el.isDone).map(el => <Todo key={el.id} el={el} />)
+
+        }
         </div>
     )
 }
